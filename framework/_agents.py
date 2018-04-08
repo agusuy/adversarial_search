6179a0c51d7d11aa1a2682cb8e2ea6e4f3eab01e
Unknown <agusuy@gmail.com>
2018-04-08 17:25:27 -0300

    reformat code

--- framework/_agents.py
+++ framework/_agents.py
@@ -1,21 +1,22 @@
 # coding=latin-1
 
 import sys
+
 from _utils import randgen
 
 
-class Agent(object): ###########################################################
-    ''' Base class for agents participating in games.
-    '''
-    
+class Agent(object):
+    """ Base class for agents participating in games.
+    """
+
     def __init__(self, name):
         self.name = name
         self.player = None
 
     def decision(self, game, *moves):
-        ''' Agents move choice. If no moves are provided, choices are retrieved
+        """ Agents move choice. If no moves are provided, choices are retrieved
             from the game.
-        '''
+        """
         if not moves:
             moves = game.moves()
             if not moves:
@@ -23,45 +24,46 @@ class Agent(object): ###########################################################
         return self._decision(moves)
 
     def _decision(self, moves):
-        ''' Method called by Agent.decision() to actually make the choice of
+        """ Method called by Agent.decision() to actually make the choice of
             move. This should be overriden by subclasses.
-        '''
-        return moves[0] # Please do not use this default implementation.
+        """
+        return moves[0]  # Please do not use this default implementation.
 
     def match_begins(self, player, game):
-        ''' Tells the agent that a match he is participating in is starting.
+        """ Tells the agent that a match he is participating in is starting.
             This should not be called again until the match ends.
-        '''
+        """
         self.player = player
 
     def match_moves(self, before, move, after):
-        ''' Tells the agent the active player have moved in the match he is
+        """ Tells the agent the active player have moved in the match he is
             participating in.
-        '''
+        """
         pass
 
     def match_ends(self, game):
-        ''' Tells the agent the match he was participating in has finished. The
+        """ Tells the agent the match he was participating in has finished. The
             game parameter holds the final game state.
-        '''
+        """
         pass
-    
+
     def __str__(self):
         return '%s(%s)' % (self.name, self.player)
-    
+
     def __hash__(self):
         return self.name.__hash__()
 
-class FileAgent(Agent): ########################################################
-    ''' An agent that takes his moves from a file and keeps record of the match
+
+class FileAgent(Agent):
+    """ An agent that takes his moves from a file and keeps record of the match
         in another one. It is also used like a user interface using standard 
         input and output.
-    '''
-    
+    """
+
     def __init__(self, in_file=None, out_file=None, name='FileAgent'):
         Agent.__init__(self, name)
         if in_file is None:
-            in_file = sys.stdin 
+            in_file = sys.stdin
         self.in_file = in_file
         if out_file is None:
             out_file = sys.stdout
@@ -88,10 +90,10 @@ class FileAgent(Agent): ########################################################
         self.out_file.flush()
 
     def _decision(self, moves):
-        ''' Writes all available moves and reads the decision from in_file. Each
+        """ Writes all available moves and reads the decision from in_file. Each
             move must be in a separate line. All lines starting with a '#' are 
             ignored.
-        '''
+        """
         self.out_file.write('# Available moves for %s: ' % self)
         self.out_file.write(' '.join([str(move) for move in moves]))
         self.out_file.write('\n')
@@ -103,23 +105,25 @@ class FileAgent(Agent): ########################################################
             line = self.in_file.readline().strip()
         return None
 
-class RandomAgent(Agent): ######################################################
-    ''' An agent that moves randomly.
-    '''
-    
+
+class RandomAgent(Agent):
+    """ An agent that moves randomly.
+    """
+
     def __init__(self, random=None, name='RandomAgent'):
         Agent.__init__(self, name)
         # An instance of random.Random or equivalent is expected, else an 
         # integer seed or None to create a random.Random.
         self.random = randgen(random)
-     
+
     def _decision(self, moves):
         return self.random.choice(moves)
 
-class TraceAgent(Agent): #######################################################
-    ''' An agent that reenacts (and records) a move trace.
-    '''
-    
+
+class TraceAgent(Agent):
+    """ An agent that reenacts (and records) a move trace.
+    """
+
     def __init__(self, trace=[], proxy=None, name='TraceAgent'):
         Agent.__init__(self, name)
         self.trace = trace
@@ -130,9 +134,9 @@ class TraceAgent(Agent): #######################################################
         self._current_move = 0
 
     def _decision(self, moves):
-        ''' Returns the next move in the current trace. Else it uses the proxy
+        """ Returns the next move in the current trace. Else it uses the proxy
             agent to get a new move, recording it in the agents trace.
-        '''
+        """
         self._current_move += 1
         if self._current_move - 1 < len(self.trace):
             return self.trace[self._current_move - 1]
@@ -141,10 +145,11 @@ class TraceAgent(Agent): #######################################################
             self.trace.append(move)
             return move
 
-class MiniMaxAgent(Agent): #####################################################
-    ''' An agent implementing simple heuristic MiniMax.
-    '''
-    
+
+class MiniMaxAgent(Agent):
+    """ An agent implementing simple heuristic MiniMax.
+    """
+
     def __init__(self, name="MiniMaxAgent", horizon=3, random=None, heuristic=None):
         Agent.__init__(self, name)
         self.horizon = horizon
@@ -157,58 +162,63 @@ class MiniMaxAgent(Agent): #####################################################
         nexts = [(move, self._minimax(game.next(move), 1)) for move in game.moves()]
         max_val = max([val for (_, val) in nexts])
         return self.random.choice([move for (move, val) in nexts if val == max_val])
-   
+
     def terminal_value(self, game, depth):
-        ''' Returns a result if node is terminal or maximum depth has been 
+        """ Returns a result if node is terminal or maximum depth has been 
             reached. Else returns None.
-        '''
+        """
         results = game.results()
         if results:
             return results[self.player]
         if depth >= self.horizon:
             return self.heuristic(game, depth)
         return None
-   
+
     def _minimax(self, game, depth):
         result = self.terminal_value(game, depth)
-        if result is None: 
+        if result is None:
             result = (max if game.active_player() == self.player else min)(
-                [self._minimax(game.next(move), depth+1) for move in game.moves()])
+                [self._minimax(game.next(move), depth + 1) for move in game.moves()])
         return result
-    
+
     def heuristic(self, game, depth):
-        ''' This method implements the heuristic for the minimax algorithm. If 
+        """ This method implements the heuristic for the minimax algorithm. If 
             no implementation is provided it returns a random value. This default
             behaviour usually should not be applied in a game.
-        '''
+        """
         return self.__heuristic__(self, game, depth) if self.__heuristic__ else self.random.random() * 2 - 1
+
 
 INFINITE = 0x7FFFFFFF
 
+
 class AlphaBetaAgent(MiniMaxAgent):
-    ''' An agent implementing MiniMax with alpha-beta pruning.
+    """ An agent implementing MiniMax with alpha-beta pruning.
         <http://en.wikipedia.org/wiki/Alpha-beta_pruning>
-    '''
-    
+    """
+
     def __init__(self, name="AlphaBetaAgent", horizon=3, random=None, heuristic=None):
         MiniMaxAgent.__init__(self, name, horizon, random, heuristic)
-    
+
     def _minimax(self, game, depth, alpha=-INFINITE, beta=INFINITE):
         result = self.terminal_value(game, depth)
-        if not result is None: return result
+        if result is not None:
+            return result
         moves = game.moves()
         active_player = game.active_player()
         if active_player == self.player:
             for move in moves:
                 value = self._minimax(game.next(move), depth + 1, alpha, beta)
-                if alpha < value: alpha = value
-                if beta <= alpha: break
+                if alpha < value:
+                    alpha = value
+                if beta <= alpha:
+                    break
             return alpha
         else:
             for move in moves:
                 value = self._minimax(game.next(move), depth + 1, alpha, beta)
-                if beta > value: beta = value
-                if beta <= alpha: break
+                if beta > value:
+                    beta = value
+                if beta <= alpha:
+                    break
             return beta
-    
-################################################################################
--- framework/test_agents.py
+++ framework/test_agents.py
@@ -1,26 +1,29 @@
 # coding=latin-1
-''' Test cases for module _agents.
-'''
-import unittest, random
+""" Test cases for module _agents.
+"""
+import random
+import unittest
 from itertools import combinations
-import _base as base, _contests as contests
+
+import _base as base
+import _contests as contests
 from _agents import RandomAgent, MiniMaxAgent, AlphaBetaAgent
-from tictactoe import TicTacToe
 from test_games import Silly
+from tictactoe import TicTacToe
 
 
 class TestAgents(unittest.TestCase):
-    ''' Basic testcases for agents behaviour.
-    '''
-    
+    """ Basic testcases for agents behaviour.
+    """
+
     def assertBetterAgent(self, agentWorse, agentBest, game, match_count=5):
         stats = contests.complete(contests.AllAgainstAll_Contest(game, [agentWorse, agentBest], match_count))
         self.assertGreater(stats.result_sum[agentBest], stats.result_sum[agentWorse])
-    
+
     def assertBetterThanRandom(self, agent, game, match_count=10, seed=None):
         randomAgent = RandomAgent(random.Random(seed if seed else agent.name.__hash__()))
         self.assertBetterAgent(randomAgent, agent, game, match_count)
-    
+
     def testSilly(self):
         game = Silly()
         # Run matches only to see if agent components fail.
@@ -29,25 +32,26 @@ class TestAgents(unittest.TestCase):
         # Statistically MiniMax based agents should beat random agents even without a proper heuristic.
         self.assertBetterThanRandom(MiniMaxAgent(), game)
         self.assertBetterThanRandom(AlphaBetaAgent(), game)
-    
+
     def testTicTacToe(self):
         rand = random.Random(123456789L)
         game = TicTacToe()
         minimax_agents = [MiniMaxAgent, AlphaBetaAgent]
-        
+
         # Statistically MiniMax based agents should beat random agents even without a proper heuristic.
         for agent in minimax_agents:
             self.assertBetterThanRandom(agent(random=rand), game)
-        
+
         # Statistically MiniMax based should improve with greater horizons.
         for agent in minimax_agents:
-            self.assertBetterAgent(agent('Horizon1', horizon=1, random=rand), 
-                agent('Horizon5', horizon=5, random=rand), game, 20)
-        
+            self.assertBetterAgent(agent('Horizon1', horizon=1, random=rand),
+                                   agent('Horizon5', horizon=5, random=rand), game, 20)
+
         # Statistically in MiniMax based agents having a simple heuristic should be better than none.
         for agent in minimax_agents:
-            self.assertBetterAgent(agent('RandomHeuristic', random=rand), 
-                agent('SimpleHeuristic', heuristic=TicTacToe.simple_heuristic, random=rand), game)
+            self.assertBetterAgent(agent('RandomHeuristic', random=rand),
+                                   agent('SimpleHeuristic', heuristic=TicTacToe.simple_heuristic, random=rand), game)
 
-if __name__ == "__main__": #####################################################
+
+if __name__ == "__main__":
     unittest.main()
