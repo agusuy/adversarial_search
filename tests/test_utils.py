5f8d300d3a1527e97ae1f567f782cf671213da6f
Agustín Castillo <agusuy@gmail.com>
2020-08-16 16:45:05 -0300

    pytest parametrize test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,109 +1,96 @@
-# -*- coding: utf-8 -*-
-import unittest
+import pytest
 
 from .context import adversarial_search as a_s
 
 
-class TestUtils(unittest.TestCase):
+class TestUtils:
     """ Test cases for utils
     """
 
-    def test_coord(self):
+    @pytest.mark.parametrize("col, row, expected_coord",
+                             [(0, 0, 'a1'),
+                              (2, 6, 'c7'),
+                              (3, 7, 'd8'),
+                              ])
+    def test_coord(self, col, row, expected_coord):
         """ Test utils.coord_id(column, row)
         """
+        assert a_s.utils.coord_id(col, row) == expected_coord
 
-        coord_id = a_s.utils.coord_id
-
-        self.assertEqual('a1', coord_id(0, 0))
-        self.assertEqual('c7', coord_id(2, 6))
-        self.assertEqual('d8', coord_id(3, 7))
-
+    def test_coord_main_diagonal(self):
+        """ Test utils.coord_id(column, row)
+        """
         columns = list(a_s.utils.__COLUMNS__)
         for i, col in zip(range(len(columns)), columns):
-            self.assertEqual(col + str(i + 1), coord_id(i, i))
+            assert col + str(i + 1) == a_s.utils.coord_id(i, i)
 
-    def test_print_board(self):
+    @pytest.mark.parametrize("board, rows, cols, row_sep, col_sep, joint_sep, row_fmt, expected_print",
+                             [('A', 1, 1, '!', '!', '!', '%s\n', 'A\n'),
+                              ('ABCD', 2, 2, '', '', '', '%s\n', 'AB\nCD\n'),
+                              ('ABCD', 2, 2, '-', '|', '', '%s\n', 'A|B\n--\nC|D\n'),
+                              ('ABCD', 2, 2, '-', '|', '+', '%s\n', 'A|B\n-+-\nC|D\n'),
+                              ('ABCD', 2, 2, '', '|', '+', '%s\n', 'A|B\nC|D\n'),
+                              ('ABCD', 2, 2, '', '', '', '|%s|\n', '|AB|\n|CD|\n'),
+                              ('ABCD', 2, 2, '-', '', '', '|%s|\n', '|AB|\n|--|\n|CD|\n'),
+                              ('ABCDEF', 3, 2, '', '', '', '%s\n', 'AB\nCD\nEF\n'),
+                              ('ABCDEF', 2, 3, '', '', '', '%s\n', 'ABC\nDEF\n'),
+                              (" " * 9, 3, 3, "-", "|", "+", '%s\n', ' | | \n-+-+-\n | | \n-+-+-\n | | \n'),
+                              ])
+    def test_print_board(self, board, rows, cols, row_sep, col_sep, joint_sep, row_fmt, expected_print):
         """ Test utils.print_board(board, rows, cols, row_sep, col_sep, joint_sep, row_fmt)
         """
+        assert a_s.utils.print_board(board, rows, cols, row_sep, col_sep, joint_sep, row_fmt) == expected_print
 
-        print_board = a_s.utils.print_board
-
-        self.assertEqual('A\n', print_board('A', 1, 1, '!', '!', '!'))
-        self.assertEqual('AB\nCD\n', print_board('ABCD', 2, 2))
-        self.assertEqual('A|B\n--\nC|D\n', print_board('ABCD', 2, 2, '-', '|'))
-        self.assertEqual('A|B\n-+-\nC|D\n', print_board('ABCD', 2, 2, '-', '|', '+'))
-        self.assertEqual('A|B\nC|D\n', print_board('ABCD', 2, 2, '', '|', '+'))
-        self.assertEqual('|AB|\n|CD|\n', print_board('ABCD', 2, 2, row_fmt='|%s|\n'))
-        self.assertEqual('|AB|\n|--|\n|CD|\n', print_board('ABCD', 2, 2, '-', '', '', row_fmt='|%s|\n'))
-        self.assertEqual('AB\nCD\nEF\n', print_board('ABCDEF', 3, 2))
-        self.assertEqual('ABC\nDEF\n', print_board('ABCDEF', 2, 3))
-
-        self.assertEqual(' | | \n-+-+-\n | | \n-+-+-\n | | \n',
-                         print_board(" " * 9, 3, 3, row_sep="-", col_sep="|", joint_sep="+"))
-
-    def test_board_orthogonals(self):
+    @pytest.mark.parametrize("board, row_num, col_num, expected_rows, expected_cols",
+                             [('A', 1, 1, ['A'], ['A']),
+                              ('ABCD', 2, 2, ['AB', 'CD'], ['AC', 'BD']),
+                              ('ABCDEFGHI', 3, 3, ['ABC', 'DEF', 'GHI'], ['ADG', 'BEH', 'CFI']),
+                              ('ABCDEF', 3, 2, ['AB', 'CD', 'EF'], ['ACE', 'BDF']),
+                              ('ABCDEF', 2, 3, ['ABC', 'DEF'], ['AD', 'BE', 'CF']),
+                              ('ABCDEF', 1, 6, ['ABCDEF'], ['A', 'B', 'C', 'D', 'E', 'F']),
+                              ('ABCDEF', 6, 1, ['A', 'B', 'C', 'D', 'E', 'F'], ['ABCDEF']),
+                              ])
+    def test_board_orthogonals(self, board, row_num, col_num, expected_rows, expected_cols):
         """ Testing orthogonal lines (functions board_rows, board_columns, board_orthogonals).
         """
-        board_rows = a_s.utils.board_rows
-        board_columns = a_s.utils.board_columns
-        board_orthogonal = a_s.utils.board_orthogonals
+        assert a_s.utils.board_rows(board, row_num, col_num) == expected_rows
+        assert a_s.utils.board_columns(board, row_num, col_num) == expected_cols
+        assert list(a_s.utils.board_orthogonals(board, row_num, col_num)) == expected_rows + expected_cols
 
-        def _test_orthogonals(board, row_num, col_num, expected_rows, expected_cols):
-            self.assertEqual(expected_rows, board_rows(board, row_num, col_num))
-            self.assertEqual(expected_cols, board_columns(board, row_num, col_num))
-            self.assertEqual(expected_rows + expected_cols, list(board_orthogonal(board, row_num, col_num)))
-
-        _test_orthogonals('A', 1, 1, ['A'], ['A'])
-        _test_orthogonals('ABCD', 2, 2, ['AB', 'CD'], ['AC', 'BD'])
-        _test_orthogonals('ABCDEFGHI', 3, 3, ['ABC', 'DEF', 'GHI'], ['ADG', 'BEH', 'CFI'])
-        _test_orthogonals('ABCDEF', 3, 2, ['AB', 'CD', 'EF'], ['ACE', 'BDF'])
-        _test_orthogonals('ABCDEF', 2, 3, ['ABC', 'DEF'], ['AD', 'BE', 'CF'])
-        _test_orthogonals('ABCDEF', 1, 6, ['ABCDEF'], ['A', 'B', 'C', 'D', 'E', 'F'])
-        _test_orthogonals('ABCDEF', 6, 1, ['A', 'B', 'C', 'D', 'E', 'F'], ['ABCDEF'])
-
-    def test_board_diagonals(self):
+    @pytest.mark.parametrize("board, row_num, col_num, expected_positive_diagonals, expected_negative_diagonals",
+                             [('A', 1, 1, ['A'], ['A']),
+                              ('ABCD', 2, 2, ['A', 'BC', 'D'], ['C', 'AD', 'B']),
+                              ('ABCDEFGHI', 3, 3, ['A', 'BD', 'CEG', 'FH', 'I'], ['G', 'DH', 'AEI', 'BF', 'C']),
+                              ('ABCDEF', 3, 2, ['A', 'BC', 'DE', 'F'], ['E', 'CF', 'AD', 'B']),
+                              ('ABCDEF', 2, 3, ['A', 'BD', 'CE', 'F'], ['D', 'AE', 'BF', 'C']),
+                              ('ABCDEF', 1, 6, ['A', 'B', 'C', 'D', 'E', 'F'], ['A', 'B', 'C', 'D', 'E', 'F']),
+                              ('ABCDEF', 6, 1, ['A', 'B', 'C', 'D', 'E', 'F'], ['F', 'E', 'D', 'C', 'B', 'A']),
+                              ])
+    def test_board_diagonals(self, board, row_num, col_num, expected_positive_diagonals, expected_negative_diagonals):
         """ Testing diagonal lines (functions board_positive_diagonals, board_negative_diagonals, board_diagonals).
         """
-        board_positive_diagonals = a_s.utils.board_positive_diagonals
-        board_negative_diagonals = a_s.utils.board_negative_diagonals
-        board_diagonals = a_s.utils.board_diagonals
+        assert a_s.utils.board_positive_diagonals(board, row_num, col_num) == expected_positive_diagonals
+        assert a_s.utils.board_negative_diagonals(board, row_num, col_num) == expected_negative_diagonals
+        assert list(a_s.utils.board_diagonals(board, row_num, col_num)) == \
+               expected_positive_diagonals + expected_negative_diagonals
 
-        def _test_diagonals(board, row_num, col_num, expected_positive_diagonals, expected_negative_diagonals):
-            self.assertEqual(expected_positive_diagonals, board_positive_diagonals(board, row_num, col_num))
-            self.assertEqual(expected_negative_diagonals, board_negative_diagonals(board, row_num, col_num))
-            self.assertEqual(expected_positive_diagonals + expected_negative_diagonals,
-                             list(board_diagonals(board, row_num, col_num)))
-
-        _test_diagonals('A', 1, 1, ['A'], ['A'])
-        _test_diagonals('ABCD', 2, 2, ['A', 'BC', 'D'], ['C', 'AD', 'B'])
-        _test_diagonals('ABCDEFGHI', 3, 3, ['A', 'BD', 'CEG', 'FH', 'I'], ['G', 'DH', 'AEI', 'BF', 'C'])
-        _test_diagonals('ABCDEF', 3, 2, ['A', 'BC', 'DE', 'F'], ['E', 'CF', 'AD', 'B'])
-        _test_diagonals('ABCDEF', 2, 3, ['A', 'BD', 'CE', 'F'], ['D', 'AE', 'BF', 'C'])
-        _test_diagonals('ABCDEF', 1, 6, ['A', 'B', 'C', 'D', 'E', 'F'], ['A', 'B', 'C', 'D', 'E', 'F'])
-        _test_diagonals('ABCDEF', 6, 1, ['A', 'B', 'C', 'D', 'E', 'F'], ['F', 'E', 'D', 'C', 'B', 'A'])
-
-    def test_board_lines(self):
+    @pytest.mark.parametrize("board, rows, cols, expected_lines", [
+        ('A', 1, 1,
+         ['A', 'A', 'A', 'A']),
+        ('ABCD', 2, 2,
+         ['AB', 'CD', 'AC', 'BD', 'A', 'BC', 'D', 'C', 'AD', 'B']),
+        ('ABCDEFGHI', 3, 3,
+         ['ABC', 'DEF', 'GHI', 'ADG', 'BEH', 'CFI', 'A', 'BD', 'CEG', 'FH', 'I', 'G', 'DH', 'AEI', 'BF', 'C']),
+        ('ABCDEF', 3, 2,
+         ['AB', 'CD', 'EF', 'ACE', 'BDF', 'A', 'BC', 'DE', 'F', 'E', 'CF', 'AD', 'B']),
+        ('ABCDEF', 2, 3,
+         ['ABC', 'DEF', 'AD', 'BE', 'CF', 'A', 'BD', 'CE', 'F', 'D', 'AE', 'BF', 'C']),
+        ('ABCDEF', 1, 6,
+         ['ABCDEF', 'A', 'B', 'C', 'D', 'E', 'F', 'A', 'B', 'C', 'D', 'E', 'F', 'A', 'B', 'C', 'D', 'E', 'F']),
+        ('ABCDEF', 6, 1,
+         ['A', 'B', 'C', 'D', 'E', 'F', 'ABCDEF', 'A', 'B', 'C', 'D', 'E', 'F', 'F', 'E', 'D', 'C', 'B', 'A']),
+    ])
+    def test_board_lines(self, board, rows, cols, expected_lines):
         """Test utils.board_lines(board, rows, cols)
         """
-
-        board_lines = a_s.utils.board_lines
-
-        self.assertEqual(['A', 'A', 'A', 'A'], list(board_lines('A', 1, 1)))
-        self.assertEqual(['AB', 'CD', 'AC', 'BD', 'A', 'BC', 'D', 'C', 'AD', 'B'], list(board_lines('ABCD', 2, 2)))
-        self.assertEqual(
-            ['ABC', 'DEF', 'GHI', 'ADG', 'BEH', 'CFI', 'A', 'BD', 'CEG', 'FH', 'I', 'G', 'DH', 'AEI', 'BF', 'C'],
-            list(board_lines('ABCDEFGHI', 3, 3)))
-        self.assertEqual(['AB', 'CD', 'EF', 'ACE', 'BDF', 'A', 'BC', 'DE', 'F', 'E', 'CF', 'AD', 'B'],
-                         list(board_lines('ABCDEF', 3, 2)))
-        self.assertEqual(['ABC', 'DEF', 'AD', 'BE', 'CF', 'A', 'BD', 'CE', 'F', 'D', 'AE', 'BF', 'C'],
-                         list(board_lines('ABCDEF', 2, 3)))
-        self.assertEqual(
-            ['ABCDEF', 'A', 'B', 'C', 'D', 'E', 'F', 'A', 'B', 'C', 'D', 'E', 'F', 'A', 'B', 'C', 'D', 'E', 'F'],
-            list(board_lines('ABCDEF', 1, 6)))
-        self.assertEqual(
-            ['A', 'B', 'C', 'D', 'E', 'F', 'ABCDEF', 'A', 'B', 'C', 'D', 'E', 'F', 'F', 'E', 'D', 'C', 'B', 'A'],
-            list(board_lines('ABCDEF', 6, 1)))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert list(a_s.utils.board_lines(board, rows, cols)) == expected_lines
