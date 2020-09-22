d6c9d881ed432b96b8f28d2dc251cfbda1a4866d
Agustín Castillo <agusuy@gmail.com>
2020-09-22 15:42:53 -0300

    fix tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,29 +1,33 @@
+from unittest.mock import patch
+
 import pytest
 
-import adversarial_search.agents.agent
 from .context import adversarial_search as a_s
+
+Game = a_s.core.Game
 
 
 class TestGame:
+    @patch.object(Game, '__abstractmethods__', set())
     def setup(self):
-        self.game = a_s.core.Game(*('A', 'B'))
+        self.game = Game(*('A', 'B'))
+
+    def test_init(self):
+        with pytest.raises(TypeError) as e:
+            Game()
+        assert "Can't instantiate abstract class %s" % Game.__name__ in str(e.value)
 
     def test_players(self):
         assert self.game.players == ('A', 'B')
 
-    @staticmethod
-    def assert_not_implemented(function, *args):
-        with pytest.raises(NotImplementedError):
-            function(*args)
-
     def test_active_player(self):
-        self.assert_not_implemented(self.game.active_player)
+        assert self.game.active_player() is None
 
     def test_moves(self):
-        self.assert_not_implemented(self.game.moves)
+        assert self.game.moves() is None
 
     def test_results(self):
-        self.assert_not_implemented(self.game.results)
+        assert self.game.results() is None
 
     def test_next(self):
-        self.assert_not_implemented(self.game.next, "move")
+        assert self.game.next("move") is None
